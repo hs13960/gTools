92e2536064f56e297ea4751bb1236aa52757fb13
tooomcat <hs87843160@hotmail.com>
2021-08-10 22:22:22 +0800

    反向修改

--- func.go
+++ func.go
@@ -13,14 +13,14 @@ func GetNowTimeStr() string{
 	return time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
 }
 
-func GetLocalIP() (string, error){
+func GetLocalIP() string{
 	conn, err := net.Dial("udp", "8.8.8.8:53")
 	if err != nil{
 		log.Printf("get local addr err:%v", err)
-		return "", err
+		return ""
 	}
 	localIp := strings.Split(conn.LocalAddr().String(), ":")[0]
-	return localIp, nil
+	return localIp
 }
 
 func GetHostName() string{
